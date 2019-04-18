5b3e6338a1e0fcb640453c8ea8baaf6faf2c6039
Unknown <imamdwicahyo29@gmail.com>
2019-04-18 12:12:15 +0700

    Second Push

--- app/src/main/java/com/idea/idc/latihan2/UserActivity.java
+++ app/src/main/java/com/idea/idc/latihan2/UserActivity.java
@@ -1,15 +1,13 @@
 package com.idea.idc.latihan2;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class UserActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
     }
-
 }
